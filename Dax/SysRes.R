0e7e301c4006ccda10b40afd742fe52ed65b2575
allanx38 <allanx38@yahoo.co.uk>
2014-05-23 07:07:31 +0100

    2205

--- Dax/SysRes.R
+++ Dax/SysRes.R
@@ -1,6 +1,13 @@
 source("D:/Allan/DropBox/RWorkingDir/Trading/Dax/addTA_fnc.R")
 setwd("D:/Allan/DropBox/RWorkingDir/Trading/Dax")
-Mkt <- read.csv("D:/Allan/DropBox/RWorkingDir/Trading/Data/Oz_ta.csv")
+Mkt <- read.csv("D:/Allan/DropBox/RWorkingDir/Trading/Data/Dax_ta.csv")
+ln <- nrow(Mkt) 
+lw <- 300
+Mkt <- Mkt[3430:ln,]
+head(Mkt)
+nrow(Mkt)
+
+tail(b[,c(1,10,13,14,16,17)],n=30)
 
 c('run',' 2')
 b <- calcT2(Mkt,2)
@@ -26,13 +33,6 @@ bb <- na.omit(b)
 mean(bb[bb$PL2>0,18])
 mean(bb[bb$PL2<0,18])
 
-c('run',' 5')
-b <- calcT2(Mkt,5)
-sum(b$PL2, na.rm=T)
-sum(b$PL2>0, na.rm=T) / (sum(b$PL2>0, na.rm=T) + sum(b$PL2<0, na.rm=T))
-bb <- na.omit(b) 
-mean(bb[bb$PL2>0,18])
-mean(bb[bb$PL2<0,18])
 
 c('run',' 6')
 b <- calcT2(Mkt,6)
